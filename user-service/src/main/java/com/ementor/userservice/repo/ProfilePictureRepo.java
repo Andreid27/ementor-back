53d817bdfcedbea0140016b1ff70e935b0a83691
andreid27 <andreid@globalsoftsys.ro>
2023-09-03 17:53:27 +0300

    spotless

--- user-service/src/main/java/com/ementor/userservice/repo/ProfilePictureRepo.java
+++ user-service/src/main/java/com/ementor/userservice/repo/ProfilePictureRepo.java
@@ -2,12 +2,11 @@
 package com.ementor.userservice.repo;
 
 import com.ementor.userservice.entity.ProfilePicture;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-import java.util.UUID;
 
 public interface ProfilePictureRepo extends JpaRepository<ProfilePicture, UUID>, JpaSpecificationExecutor<ProfilePicture> {
 	@Transactional
