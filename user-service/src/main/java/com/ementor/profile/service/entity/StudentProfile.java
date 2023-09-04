37b5867a4eab6552fdecfcfe8c61aa98e925fd2c
andreid27 <andreid@globalsoftsys.ro>
2023-09-04 21:35:39 +0300

    spotless after adding school speciality

--- user-service/src/main/java/com/ementor/profile/service/entity/StudentProfile.java
+++ user-service/src/main/java/com/ementor/profile/service/entity/StudentProfile.java
@@ -5,13 +5,12 @@ import com.ementor.profile.service.core.entity.CommonEntity;
 import com.ementor.profile.service.enums.SchoolDomainEnum;
 import com.ementor.profile.service.enums.SchoolSpecialityEnum;
 import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
